7889051888bcf1acf505ed1d8977cb7798348af9
Renan Couto <rennanccouto@gmail.com>
2020-04-09 16:40:13 -0300

    Commit correçao

--- avsistemasdist/src/questao1/Main.java
+++ avsistemasdist/src/questao1/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ClasseThread cls = new ClasseThread("nao consigo mais", 500);
-		ClasseThread cls1 = new ClasseThread("quero acabar isso logo", 1000);
-		ClasseThread cls2 = new ClasseThread("provinha dificil", 2000);
+		ThreadNome cls = new ThreadNome("nao consigo mais", 500);
+		ThreadNome cls1 = new ThreadNome("quero acabar isso logo", 1000);
+		ThreadNome cls2 = new ThreadNome("provinha dificil", 2000);
 		Thread th1 = new Thread(cls);
 		Thread th2 = new Thread(cls1);
 		Thread th3 = new Thread(cls2);
--- avsistemasdist/src/questao1/ThreadNome.java
+++ avsistemasdist/src/questao1/ThreadNome.java
@@ -1,12 +1,12 @@
 package questao1;
 
-public class ClasseThread implements Runnable {
+public class ThreadNome implements Runnable {
 	
 	
 	private String nome;
 	private long itempo;
 	
-	public ClasseThread(String nome, long itempo) {
+	public ThreadNome(String nome, long itempo) {
 		this.nome = nome;
 		this.itempo = itempo;
 	}
